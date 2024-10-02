7ed33ca456e2b0a91a095113a19a9d1bf040f529
naveen reddy <naveenreddy20001@gmail.com>
2024-10-02 11:48:08 +0530

    first commit

---
+++ eclipse-workspace/E-CommerceWebSite/src/main/java/com/crm/qa/base/TestBase.java
@@ -0,0 +1,21 @@
+package com.crm.qa.base;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+
+public class TestBase {
+static WebDriver driver;
+static Properties prop;
+	
+	public TestBase() {
+		try {
+			prop = new Properties();
+			FileInputStream ip = new FileInputStream(
+					System.getProperty("usr.dir")+"");
+			prop.load(ip);			
+		}atch 
+		
+	}
+}
---
+++ eclipse-workspace/E-CommerceWebSite/src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -0,0 +1,16 @@
+package com.crm.qa.testcases;
+
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	@Test
+	public void test() {
+		System.out.println("ghhjfh");
+	}
+
+}
